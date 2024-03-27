baa140d156cefceafad09d4a26e78f9887307a4f
Alex Sklar <sklarfox@gmail.com>
2024-03-27 12:23:58 -0700

    feat(init): wrap create Vite command
    Co-authored-by: Erik Margetis <erik.margetis@gmail.com>
    Co-authored-by: Joseph Liang <j3liang@icloud.com>
    Co-authored-by: Franco Waisfeld <Francowaisfeld@gmail.com>

--- src/commands/init.ts
+++ src/commands/init.ts
@@ -1,30 +1,42 @@
-import {Args, Command, Flags} from '@oclif/core'
+import {Command, ux} from '@oclif/core'
+import {execa} from 'execa'
+import {input} from '@inquirer/prompts'
 
 export default class Init extends Command {
-  static args = {
-    file: Args.string({description: 'file to read'}),
-  }
-
-  static description = 'describe the command here'
-
-  static examples = [
-    '<%= config.bin %> <%= command.id %>',
-  ]
-
-  static flags = {
-    // flag with no value (-f, --force)
-    force: Flags.boolean({char: 'f'}),
-    // flag with a value (-n, --name=VALUE)
-    name: Flags.string({char: 'n', description: 'name to print'}),
-  }
+  static description = 'Create a fresh React app, preconfigured with a Syncosaurus multiplayer backend.'
 
   public async run(): Promise<void> {
-    const {args, flags} = await this.parse(Init)
+    this.log('🦖 Creating a new Syncosaurus backed React app!')
+    const projectName = await input({message: 'What is the name of your project?'})
 
-    const name = flags.name ?? 'world'
-    this.log(`hello ${name} from /Users/alex/git/syncosaurus-cli/src/commands/init.ts`)
-    if (args.file && flags.force) {
-      this.log(`you input --force and --file: ${args.file}`)
-    }
+    const process = execa('npm', ['create', 'vite@latest', projectName, '--', '--template', 'react'], {
+      stdin: 'inherit',
+    })
+
+    ux.action.start('🦖🧙 Engaging dino wizardry to scaffold your project')
+
+    let viteCompleted = false
+    let finalOutput = ''
+    process.stdout?.on('data', async (data) => {
+      const str = data.toString()
+      if (str.includes('is not empty.')) {
+        this.error('❌ It looks like that project name already exists. Please try again with a new one.\n\n')
+      }
+
+      if (str.includes('Done')) {
+        viteCompleted = true
+      }
+
+      if (viteCompleted) {
+        finalOutput += str
+      }
+    })
+
+    // Resolves once Vite has completed scaffolding a project
+    await process
+
+    // Log the final output from running Vite
+    ux.action.stop('Finished!\n')
+    this.log(finalOutput.slice(6))
   }
 }
