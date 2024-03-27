52606dade4fdedcb8142b945fff4e4d2e091bd27
Alex Sklar <sklarfox@gmail.com>
2024-03-26 18:30:24 -0700

    feat(dev): add in dev command to spin up vite and wrangler dev servers
    Co-authored-by: Erik Margetis <erik.margetis@gmail.com>
    Co-authored-by: Joseph Liang <j3liang@icloud.com>
    Co-authored-by: Franco Waisfeld <Francowaisfeld@gmail.com>

---
+++ src/commands/dev.ts
@@ -0,0 +1,19 @@
+import {Command} from '@oclif/core'
+import {execa} from 'execa'
+
+export class MyCommand extends Command {
+  static description = 'description of this example command'
+
+  async run(): Promise<void> {
+    const {stdout} = await execa('ls')
+
+    if (stdout.includes('syncosaurus.json')) {
+      const viteProcess = execa('vite', {stdio: 'inherit'})
+      const wranglerProcess = execa('wrangler', ['dev'], {stdio: 'inherit'})
+      Promise.all([viteProcess, wranglerProcess])
+      this.log('🦖 Dev servers shutting down!')
+    } else {
+      this.log("🦖 Error! It looks like you aren't in a Syncosaurus project.")
+    }
+  }
+}
