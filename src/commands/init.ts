f6a65a699f17bbe013be9f8b0e3c47bc0c27b210
Alex Sklar <sklarfox@gmail.com>
2024-03-27 13:57:32 -0700

    feat(init): create init command
    This command scaffolds a project using vite and installs syncosaurus as a dependency.
    
    Co-authored-by: Erik Margetis <erik.margetis@gmail.com>
    Co-authored-by: Joseph Liang <j3liang@icloud.com>
    Co-authored-by: Franco Waisfeld <Francowaisfeld@gmail.com>

--- src/commands/init.ts
+++ src/commands/init.ts
@@ -1,23 +1,27 @@
 import {Command, ux} from '@oclif/core'
 import {execa} from 'execa'
 import {input} from '@inquirer/prompts'
+import {generateWranglerToml, generateSyncoJson} from '../utils/configs.js'
 
 export default class Init extends Command {
   static description = 'Create a fresh React app, preconfigured with a Syncosaurus multiplayer backend.'
 
   public async run(): Promise<void> {
+    const cwd = process.cwd()
+
+    // Query for project name
     this.log('🦖 Creating a new Syncosaurus backed React app!')
     const projectName = await input({message: 'What is the name of your project?'})
+    const projectDir = process.cwd() + '/' + projectName
 
-    const process = execa('npm', ['create', 'vite@latest', projectName, '--', '--template', 'react'], {
-      stdin: 'inherit',
-    })
-
-    ux.action.start('🦖🧙 Engaging dino wizardry to scaffold your project')
-
+    // Create the vite project
+    ux.action.start('🧙 Engaging dino wizardry to scaffold your project')
     let viteCompleted = false
     let finalOutput = ''
-    process.stdout?.on('data', async (data) => {
+    const createVite = execa('npm', ['create', 'vite@latest', projectName, '--', '--template', 'react'], {
+      stdin: 'inherit',
+    })
+    createVite.stdout?.on('data', async (data) => {
       const str = data.toString()
       if (str.includes('is not empty.')) {
         this.error('❌ It looks like that project name already exists. Please try again with a new one.\n\n')
@@ -31,12 +35,34 @@ export default class Init extends Command {
         finalOutput += str
       }
     })
+    await createVite
+    ux.action.stop('Finished!')
 
-    // Resolves once Vite has completed scaffolding a project
-    await process
+    // Install syncosaurus as a dependency
+    ux.action.start('🦖 Integrating Syncosaurus')
+    await execa('npm', ['install', 'syncosaurus'], {
+      cwd: projectDir,
+    })
+
+    // Create the syncosaurus config file
+    await execa(`echo '${generateSyncoJson(projectName)}' > 'syncosaurus.json'`, [], {
+      cwd: projectDir,
+      shell: true,
+      stdio: 'inherit',
+    })
 
     // Log the final output from running Vite
     ux.action.stop('Finished!\n')
     this.log(finalOutput.slice(6))
   }
 }
+
+/*
+  Steps to do:
+
+  scaffold a vite project DONE
+  add syncosaurus as a dependency in the projects package.json DONE
+  Create a syncosaurus.json file
+  Create a wrangler.toml
+
+*/
---
+++ src/utils/configs.ts
@@ -0,0 +1,24 @@
+export const generateWranglerToml = (projectName: string) => {
+  return `name = "${projectName}"
+  main = "./index.mjs"
+  compatibility_date = "2024-03-27"
+  
+  [[durable_objects.bindings]]
+  name = "WEBSOCKET_SERVER"
+  class_name = "WebSocketServer"
+  
+  [[migrations]]
+  tag = "v1"
+  new_classes = ["WebSocketServer"]`
+}
+
+export const generateSyncoJson = (projectName: string) => {
+  return `
+  {
+    "projectName": "${projectName}",
+    "compatibilityDate": "${new Date().toISOString().slice(0, 10)}",
+    "encryption": false,
+    "authentication": false
+  }
+  `
+}
