aabfcc729aa600f4c1862da3b9041607b1dfd5ad
Alex Sklar <sklarfox@gmail.com>
2024-03-27 21:19:34 -0700

    feat(demo): add template directory for vite demo
    Co-authored-by: Erik Margetis <erik.margetis@gmail.com>
    Co-authored-by: Joseph Liang <j3liang@icloud.com>
    Co-authored-by: Franco Waisfeld <Francowaisfeld@gmail.com>

--- src/commands/dev.ts
+++ src/commands/dev.ts
@@ -2,14 +2,14 @@ import {Command} from '@oclif/core'
 import {execa} from 'execa'
 
 export class MyCommand extends Command {
-  static description = 'Start oncurrent Vite and Wrangler dev servers'
+  static description = 'Start concurrent Vite and Wrangler dev servers'
 
   async run(): Promise<void> {
     const {stdout} = await execa('ls')
 
     if (stdout.includes('syncosaurus.json')) {
       const viteProcess = execa('vite', {stdio: 'inherit'})
-      const wranglerProcess = execa('wrangler', ['dev'], {stdio: 'inherit'})
+      const wranglerProcess = execa('wrangler', ['dev', './node_modules/syncosaurus/do/index.mjs'], {stdio: 'inherit'})
       await Promise.all([viteProcess, wranglerProcess])
     } else {
       this.log("🦖 Error! It looks like you aren't in a Syncosaurus project root directory.")
