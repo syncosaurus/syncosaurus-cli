21325d0cf3dfdc87aa9f02d82dafa15c9e876bf7
Alex Sklar <sklarfox@gmail.com>
2024-03-27 23:39:48 -0700

    bug(login-check): fix bug where it was falsely returning user not logged in
    Co-authored-by: Erik Margetis <erik.margetis@gmail.com>
    Co-authored-by: Joseph Liang <j3liang@icloud.com>
    Co-authored-by: Franco Waisfeld <Francowaisfeld@gmail.com>

--- src/utils/login-check.ts
+++ src/utils/login-check.ts
@@ -1,40 +1,10 @@
-import { $ } from 'execa'
+import {execa} from 'execa'
 
 const checkLogin = async () => {
-  const cmdOutput: string[] = [];
+  const {stdout} = await execa('wrangler', ['whoami'])
+  const email = String(stdout.match(/\w+@.+[^!]/))
+  const loginStatus = stdout.includes('You are logged in with the email')
 
-  const child = $({
-    cwd: process.cwd(),
-    detached: true,
-    encoding: 'utf8',
-    env: process.env,
-    shell: true,
-    stdio: ['inherit', 'pipe', 'pipe'],
-  })`npm run whoami`;
-
-  child.stdout!.on('data', (data) => {
-    cmdOutput.push(data.toString())
-  });
-
-  return new Promise((resolve, _reject) => {
-    child.on('close', (_code) => {
-      const loginSnippet = 'You are logged in';
-      const result = cmdOutput.find((line) => line.includes(loginSnippet));
-
-      if (result) {
-        const email = result.slice(result.indexOf('email') + 5 - result.length - 2).trim();
-
-        resolve({
-          email,
-          loginStatus: true,
-        });
-      } else {
-        resolve({
-          loginStatus: false,
-        });
-      }
-    });
-  });
+  return {loginStatus, email}
 }
-
-export default checkLogin;
+export default checkLogin
