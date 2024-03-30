d4e54b69aaf271943da9bfa5b13cf06ccea06408
Alex Sklar <sklarfox@gmail.com>
2024-03-30 11:05:27 -0700

    feat(dashboard): add command to install and run local developer dashboard
    Co-authored-by: Erik Margetis <erik.margetis@gmail.com>
    Co-authored-by: Franco Waisfeld <Francowaisfeld@gmail.com>
    Co-authored-by: Joseph Liang <j3liang@icloud.com>

---
+++ test/commands/dashboard.test.ts
@@ -0,0 +1,17 @@
+import {expect, test} from '@oclif/test'
+
+describe('dashboard', () => {
+  test
+  .stdout()
+  .command(['dashboard'])
+  .it('runs hello', ctx => {
+    expect(ctx.stdout).to.contain('hello world')
+  })
+
+  test
+  .stdout()
+  .command(['dashboard', '--name', 'jeff'])
+  .it('runs hello --name jeff', ctx => {
+    expect(ctx.stdout).to.contain('hello jeff')
+  })
+})
