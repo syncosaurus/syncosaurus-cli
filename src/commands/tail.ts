9dde0076d04985fa0d739c1c4f9bfee3c6521fd3
Alex Sklar <sklarfox@gmail.com>
2024-03-30 11:37:13 -0700

    feat(tail): add wrapper command for wrangler tail
    command detects the project name and sends the appropriate tail request to cloudflare for the user
    
    Co-authored-by: Erik Margetis <erik.margetis@gmail.com>
    Co-authored-by: Franco Waisfeld <Francowaisfeld@gmail.com>
    Co-authored-by: Joseph Liang <j3liang@icloud.com>

---
+++ src/commands/tail.ts
@@ -0,0 +1,28 @@
+import {Command, ux} from '@oclif/core'
+import fs from 'fs'
+import {execa} from 'execa'
+
+export default class Tail extends Command {
+  static description = 'Setup a tail log to a deployed Syncosaurus server.'
+
+  public async run(): Promise<void> {
+    const {stdout} = await execa('ls')
+
+    if (!stdout.includes('syncosaurus.json')) {
+      this.log('Syncosaurus config file not found. Please try again in a Syncosaurus project.')
+      return
+    }
+
+    let tailProcess
+    try {
+      const jsonData = fs.readFileSync(process.cwd() + '/syncosaurus.json', 'utf8')
+      const {projectName} = JSON.parse(jsonData)
+
+      tailProcess = execa('wrangler', ['tail', projectName], {shell: true, stdin: 'inherit', stdout: 'pipe'})
+    } catch (e) {
+      this.log('There was an error.', e)
+    }
+
+    await tailProcess
+  }
+}
---
+++ test/commands/tail.test.ts
@@ -0,0 +1,17 @@
+import {expect, test} from '@oclif/test'
+
+describe('tail', () => {
+  test
+  .stdout()
+  .command(['tail'])
+  .it('runs hello', ctx => {
+    expect(ctx.stdout).to.contain('hello world')
+  })
+
+  test
+  .stdout()
+  .command(['tail', '--name', 'jeff'])
+  .it('runs hello --name jeff', ctx => {
+    expect(ctx.stdout).to.contain('hello jeff')
+  })
+})
