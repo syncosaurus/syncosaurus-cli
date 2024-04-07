d760e117e1d9cbfcf2f1b58a8f2eaae545ce2f1d
jgoteam <j3liang@icloud.com>
2024-04-07 12:32:14 -0700

    style: streamline 'syncosaurus logout' flow with other commands
    Co-authored-by: Alex Sklar <sklarfox@gmail.com>
    Co-authored-by: Erik Margetis <erik.margetis@gmail.com>

--- src/commands/logout.ts
+++ src/commands/logout.ts
@@ -1,31 +1,27 @@
-import { Command } from '@oclif/core'
-import { execaCommandSync } from 'execa'
-import ora from 'ora'
-import checkLogin from '../utils/login-check.js'
+import { Command } from '@oclif/core';
+import { execaCommandSync } from 'execa';
+import ora from 'ora';
+import checkLogin from '../utils/login-check.js';
 
 export default class Logout extends Command {
-  static description = 'Logout of Syncosaurus'
+  static description = 'Logout of Syncosaurus';
   public async run(): Promise<void> {
     interface LoginResult {
       loginStatus: boolean
       email?: string
     }
 
-    const logoutSpinner = ora('Logging out').start()
-    const loginResult: LoginResult = (await checkLogin()) as LoginResult
+    const logoutSpinner = ora('Logging out...').start();
+    const loginResult: LoginResult = (await checkLogin()) as LoginResult;
 
     if (loginResult.loginStatus) {
-      execaCommandSync(`npx wrangler logout`)
+      execaCommandSync(`npx wrangler logout`);
 
-      logoutSpinner.stopAndPersist({
-        symbol: '✅',
-        text: `You successfully logged out.`,
-      })
+      logoutSpinner.stopAndPersist({ text: 'Logging out...done! ✅' });
+      this.log('You have successfully logged out');
     } else {
-      logoutSpinner.stopAndPersist({
-        symbol: '❌',
-        text: `You are already logged out!`,
-      })
+      logoutSpinner.stopAndPersist({ text: 'Logging out...failed! ❌' });
+      this.log('You did not successfully log out');
     }
   }
 }
